bfdecbf07b50b0cafa0f6e835631b67407cf1942
Evgenii <evgenii.shevchenko@outlook.com>
2021-10-04 22:01:53 +0300

    fixed commit

---
+++ src/views/Decorators.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+   
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Decorators",
+  mounted(){
+      
+function revert(...args: any){
+    console.log(...args);
+}
+
+class User {
+    a = "SimpleName";
+    @revert
+    b = this.a;
+};
+
+const newUser = new User();
+
+console.log(":rocket: ~ file: test.js ~ line 26 ~ newUser", newUser.a);
+console.log(":rocket: ~ file: test.js ~ line 28 ~ newUser", newUser.b);
+  }
+});
+</script>
